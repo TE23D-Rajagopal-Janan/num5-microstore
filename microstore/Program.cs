1bbefca7ce4fafaef49d745d96b49b4c30d7c16a
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2024-10-11 10:56:20 +0200

    dfds

---
+++ microstore/Program.cs
@@ -0,0 +1,40 @@
+﻿int money = 125; 
+string köpval1;
+
+//int Moneyamount = 0;
+
+//int.TryParse(money, out money);
+
+while (money > 0){
+Console.WriteLine($"Du har {money} cash" );
+Console.WriteLine("Butik meny");
+Console.WriteLine("1)Kniv = 50kr");
+Console.WriteLine("2)Basebollträ = 35kr");
+Console.WriteLine("3)Gun = 100kr");
+
+köpval1 = Console.ReadLine();
+if köpval1 = 1 ();
+}
+
+
+Console.ReadLine();
+
+//Console.WriteLine("Skiv in din ålder");
+//string age = Console.ReadLine();
+
+//int ageNum = 0;
+
+//int.TryParse(age, out ageNum);
+
+//bool success = int.TryParse(age, out ageNum);
+
+//if (success == false)
+//{
+//    Console.WriteLine("No a number mate");
+//}
+//int i = 0;
+//while (i < 10)
+//{
+//Console.WriteLine("igen");
+//i++;
+//}
